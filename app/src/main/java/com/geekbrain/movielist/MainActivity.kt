62f0519bdf08cf66f8e8ce2d2773adad221a9401
paterdimitry <paterdimitry@gmail.com>
2021-05-04 09:48:09 +0300

    Initial commit

---
+++ app/src/main/java/com/geekbrain/movielist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.geekbrain.movielist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geekbrain.movielist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
